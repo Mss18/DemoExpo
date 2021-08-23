a2b2154498d56e7a2a35fe2ad9f6fd7a23500c11
Mss18 <mssonfire@gmail.com>
2021-08-23 20:16:08 +0200

    Editar y nuevo formulario en proceso

--- frontend/src/components/Editar.js
+++ frontend/src/components/Editar.js
@@ -1,5 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router-dom';
 
 export const Editar = () => {
-    return <div className="container">Editar</div>
+    const [usuario, setUsuario] = useState()
+    const { register, handleSubmit } = useForm({
+        defaultValues: { nome: usuario ? usuario.nome: "" },
+    })
+    const history = useHistory()
+
+    useEffect(() => {
+        setUsuario({
+            nome: "foo",
+            apelido: "bar",
+            anoNacemento: "123"
+        })        
+    }, [])
+
+    const onSubmit = handleSubmit((data) => {
+        alert(JSON.stringify(data))
+    })
+    return (<div className="container">
+        <div className="mt-3">
+            <h3>Editar usuario</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label type="text">Nome:</label>
+                    <input className="form-control" ref={register} type="text" name="nome" id="nome" />
+                </div>
+                <div className="form-group">
+                    <label type="text">Apelido:</label>
+                    <input className="form-control" ref={register} type="text" name="apelido" id="apelido" />
+                </div>
+                <div className="form-group">
+                    <label type="text">Ano de nacemento:</label>
+                    <input className="form-control" ref={register} type="text" name="anoNacemento" id="anoNacemento" />
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-primary">
+                        Gardar
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>)
 }
--- frontend/src/components/Listado.js
+++ frontend/src/components/Listado.js
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Listado = () => {
-    return <div className="container">Listado</div>
-}
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        setItems([
+            {text: "foo", id: 0},
+            {text: "bar", id: 1},
+            {text: "bazz", id: 2}
+        ])
+    })
+
+    return (
+    <div className="container">Listado
+    <div className="mt-3">
+        <h3>Lista de Usuarios</h3>
+        <table className="table table-striped mt-3">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Apelido</th>
+                    <th>Ano de nacemento</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+items.map(usuario => (
+    <tr key={usuario.id}>
+<td>
+    {usuario.nome}
+</td>
+<td>
+    <Link to={`/editar/${usuario.id}`}>Editar</Link>
+</td>
+    </tr>
+))
+                }
+            </tbody>
+        </table>
+    </div>
+    </div>
+    )}
